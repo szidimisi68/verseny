07d1a1acf168091faadfd27c8de6742e4a72f4c7
szidimisi68 <70961855+szidimisi68@users.noreply.github.com>
2021-11-15 10:50:41 +0100

    Update dusza.py

--- dusza.py
+++ dusza.py
@@ -1,5 +1,6 @@
 belepesi_valasz = input("belép vagy regisztrál? ")
 karakter_ellenor = ["a","A","b","B","c","C","d","D","e","E","f","F","g","G","h","H","i","I","j","J","k","K","l","L","m","M","n","N","o","O","p","P","q","Q","r","R","s","S","t","T","u","U","v","V","w","W","x","X","y","Y","z","Z","1","2","3","4","5","6","7","8","9","0"]
+szamok_lista = ["1","2","3","4","5","6","7","8","9","0"]
 karakterek_szama = 0
 felhasznalo_nevek = ["alma", "körte", "medve"]
 jelszavak = ["fafafa", "szalma", "disznó"]
@@ -11,11 +12,20 @@ n = 0
 def belepesteszt():
     global n
     n = 0
-    while siker == False:
+    while not siker:
+        adatok = open("adatok", "w")
         felhasznalo_nev = input("Felhasznaló neved: ")
         jelszo = input("Jelszavad: ")
         for fel in felhasznalo_nevek:
-            if felhasznalo_nevek[n] == felhasznalo_nev and jelszavak[n] == jelszo:
+            proba = False
+            while not proba:
+                a = str(adatok.readline())
+                if a[-5] == " ":
+                    temp_felhasznalo = a[0:-5]
+                elif a[-3] in szamok_lista:
+                    temp_jelszo = a[-4:-1]
+                    proba = True
+            if temp_felhasznalo == felhasznalo_nev and temp_jelszo == jelszo:
                 belepes()
             n += 1
         if siker == False:
@@ -52,6 +62,9 @@ def regisztral():
                 print("8 és 10 karakter hosszúság között lehet!")
                 regisztral()
                 break
+        if len(jelszo) < 10:
+            while len(jelszo) == 10:
+                jelszo = jelszo + "d"
         helyes_jelszo = True
         return jelszo
 
@@ -68,6 +81,8 @@ if belepesi_valasz == "regisztrál":
         jelszo_2 = input("jelvasza újra: ")
 
     print("Sikeresen regisztrált egy felhasználót!")
+    file = open("adatok" "w")
+    file.write(felhasznalo_nev + " " + str(ord(jelszo)) + "\n")
 
 if belepesi_valasz == "belép":
     belepesteszt()
